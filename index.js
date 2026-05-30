9d7b4b13f825f9e00d09c5bd8828efc935768001
Lakshy077 <lakshy7609@gmail.com>
2026-05-30 20:30:15 +0530

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const bedrock = require('bedrock-protocol');
 
 const bot = bedrock.createClient({
-  host: 'play.applemc.fun'
+  host: 'play.applemc.fun',
   port: 19132,
-  username: 'Gamerboy83894'
+  username: 'gamerpro453'
 });
 
-bot.on('join', () => {
-  console.log('Bot joined!');
+bot.on('connect', () => {
+  console.log('Bot connected!');
 });
 
-bot.on('text', (packet) => {
-  const msg = packet.message || '';
-
-  if (msg.includes('!accept')) {
-    bot.queue('command_request', {
-      command: '/tpaccept',
-      version: 1,
-      origin: {
-        type: 0,
-        uuid: '',
-        request_id: ''
-      },
-      internal: false
-    });
-  }
+bot.on('spawn', () => {
+  console.log('Bot joined world!');
 });
