a6d976d27955d0ae3ed93d92208e0564767ec06a
Lakshy077 <lakshy7609@gmail.com>
2026-05-30 20:16:29 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const bedrock = require('bedrock-protocol');
+
+const bot = bedrock.createClient({
+  host: play.applemc.fun,
+  port: 19132,
+  username: 'AFKBot'
+});
+
+bot.on('join', () => {
+  console.log('Bot joined!');
+});
+
+bot.on('text', (packet) => {
+  const msg = packet.message || '';
+
+  if (msg.includes('!accept')) {
+    bot.queue('command_request', {
+      command: '/tpaccept',
+      version: 1,
+      origin: {
+        type: 0,
+        uuid: '',
+        request_id: ''
+      },
+      internal: false
+    });
+  }
+});
